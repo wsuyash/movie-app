f8b6fdb8bca752f0a2fff638057ae184be662e76
wsuyash <suyashcr07@gmail.com>
2022-01-06 05:03:46 +0530

    Add styles, Navbar and MovieCard component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
+import { data } from '../data';
+import MovieCard from './MovieCard';
+import Navbar from './Navbar';
+
 function App() {
   return (
     <div className="App">
-			pro setup
+			<Navbar />
+			<div className="main">
+
+				<div className="tabs">
+					<div className="tab">Movies</div>
+					<div className="tab">Favourites</div>
+				</div>
+
+				<div className="list">
+					{data.map((movie) => (
+						<MovieCard movie={movie} />					
+					))}
+				</div>
+
+			</div>
     </div>
   );
 }
