a29071db976074fd9708f9ae488638172c553f0f
wsuyash <suyashcr07@gmail.com>
2022-01-06 05:28:21 +0530

    Add redux package and create Store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+
+import "./index.css";
+import App from "./components/App";
+import movies from "./reducers";
+
+
+const store = createStore(movies);
+console.log('store', store);
+console.log('STATE', store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
